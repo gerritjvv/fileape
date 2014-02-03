d8a0738333de5b13ba3d7bff6c213ce734e11ad7
gerritjvv <gerritjvv@gmail.com>
2014-02-03 12:51:14 +0100

    up fun-utils

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                 *assert* false}
 
   :dependencies [
-                  [fun-utils "0.3.6"]
+                  [fun-utils "0.4.0"]
                   [org.clojure/tools.logging "0.2.3"]
                   [org.iq80.snappy/snappy "0.3"]
                   [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
