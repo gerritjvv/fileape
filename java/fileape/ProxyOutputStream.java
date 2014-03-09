a666b70c98d0ff2b6c8262fbda8baf5ca256f3ba
gerritjvv <gerritjvv@gmail.com>
2014-03-10 00:41:01 +0100

    use DataOutputStream

--- java/fileape/ProxyOutputStream.java
+++ java/fileape/ProxyOutputStream.java
@@ -1,45 +1,19 @@
 package fileape;
 
-import java.io.IOException;
+import java.io.DataOutputStream;
 import java.io.OutputStream;
 
 /**
  * 
  * 
  */
-public class ProxyOutputStream extends OutputStream{
+public class ProxyOutputStream extends DataOutputStream {
 
 	final OutputStream out;
 
 	public ProxyOutputStream(OutputStream out) {
-		super();
+		super(out);
 		this.out = out;
 	}
 
-	@Override
-	public void write(int b) throws IOException {
-		out.write(b);
-	}
-
-	@Override
-	public void write(byte[] b) throws IOException {
-		out.write(b);
-	}
-
-	@Override
-	public void write(byte[] b, int off, int len) throws IOException {
-		out.write(b, off, len);
-	}
-
-	@Override
-	public void flush() throws IOException {
-		out.flush();
-	}
-
-	@Override
-	public void close() throws IOException {
-		out.close();
-	}
-	
-	
 }
