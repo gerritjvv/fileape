1ec322f39e25705a2e28c3e46c1ffb09b437c946
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2014-08-12 16:23:47 +0200

    FIX OOM add remove to start channel

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fileape "0.6.4"
+(defproject fileape "0.6.5"
   :description "Write data to files split by topic and rolled over on size or a timeout, files can be compressed using lzo, snappy or gzip"
   :url "https://github.com/gerritjvv/fileape"
   :license {:name "Eclipse Public License"
